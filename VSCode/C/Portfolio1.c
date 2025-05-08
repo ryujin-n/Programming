16edf0ba019d5391910defde2eb29f0f3a710a71
Miles <vegasfh07@gmail.com>
2025-05-08 17:49:17 -0300

    blee

--- VSCode/C/Portfolio1.c
+++ VSCode/C/Portfolio1.c
@@ -2,47 +2,41 @@
 #include <stdlib.h>
 #include <locale.h>
 
-    int main(){
-        // declarar variáveis de entrada
+int main() {
+    // declarar variáveis de entrada
+    char nome_animal[30];
+    int qnt_animal;
+    double custo_comida;
+    double qnt_comida;
 
-        char nome_animal[30];
-        int qnt_animal;
-        float custo_comida;
-        float qnt_comida;
+    // declarar variáveis de saída
+    double custo_dia;
+    double qnt_comida_mes;
+    double custo_mes;
+    
+    setlocale(LC_ALL, "portuguese");
+    
+    // query
+    printf("Informe o nome do animal: ");
+    scanf("%s", nome_animal);
+    printf("Informe a Quantidade de animais: ");
+    scanf("%d", &qnt_animal);  
+    printf("Informe o custo de alimentação: ");
+    scanf("%lf", &custo_comida);
+    printf("Informe a quantidade de alimentação: ");
+    scanf("%lf", &qnt_comida);
 
-        // declarar variáveis de saída
+    // calculos
+    custo_dia = qnt_animal * custo_comida * qnt_comida;
+    qnt_comida_mes = qnt_animal * qnt_comida * 30; 
+    custo_mes = custo_dia * 30;
 
-        float custo_dia;
-        float qnt_comida_mes;
-        float custo_mes;
-        
-        setlocale(LC_ALL,"portuguese");
-        
-        // query
+    // resultados
+    printf("Animal: %s\n", nome_animal);
+    printf("Custo estimado p/dia: R$ %.2f\n", custo_dia);
+    printf("Qnt de comida consumida p/mês(kg): %.2f\n", qnt_comida_mes);
+    printf("Custo estimado p/mês: %.2f\n", custo_mes);
 
-        printf("Informe o nome do animal: ");
-        scanf("%s",nome_animal);
-        printf("Informe a Quantidade de animais: ");
-        scanf("%f",&qnt_animal);
-        printf("Informe o custo de alimentação: ");
-        scanf("%f",&custo_comida);
-        printf("Informe a quantidade de alimentação: ");
-        scanf("%f",&qnt_comida);
-
-        // calculos
-
-        custo_dia = qnt_animal * custo_comida * qnt_comida;
-        qnt_comida_mes = qnt_animal * qnt_comida * 30; 
-        custo_mes = custo_dia * 30;
-
-        // resultados
-
-        printf("Animal: %s\n",nome_animal);
-        printf("Custo estimado p/dia: R$ %.2f\n",custo_dia);
-        printf("Qnt de comida consumida p/mês(kg): %f\n",qnt_comida_mes);
-        printf("Custo estimado p/mês: R$ %.2f\n",custo_mes);
-
-        system("pause");
-        return 0;
-    }
-
+    system("pause");
+    return 0;
+}
